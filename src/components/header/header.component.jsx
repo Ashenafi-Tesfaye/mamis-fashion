c8d088cf1d38b08f33ee292001a896fd6159ddb5
Ashenafi-Tesfaye <rashenafibab@gmail.com>
2021-01-30 11:44:37 -0800

    added google sign in with a style

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './header.styles.scss';
 import {Link} from 'react-router-dom';
+import {auth} from '../firebase/firebase.utils';
 
 import {ReactComponent as Logo} from '../assets/crown.svg';
 
-const Header = ()=>(
+const Header = ({currentUser})=>(
 
     <div className='header'>
         <Link className='logo-container' to="/">
@@ -18,6 +19,15 @@ const Header = ()=>(
             CONTACT
             </Link>
         </div>
+       {
+           currentUser?(
+           <div className='options' onClick={()=>auth.signOut()}> SIGN OUT
+               </div>
+            ) : (
+            <Link className='option' to='/signin'>
+                SIGN IN
+            </Link>
+             ) }
     </div>
 );
 
