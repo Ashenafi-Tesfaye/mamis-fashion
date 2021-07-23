9cd43517a9963cf976f757780367a5525eb91e05
Ashenafi-Tesfaye <rashenafibab@gmail.com>
2021-07-22 20:30:58 -0700

    just added contact info

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Contact from './pages/contact/contact.jsx';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
@@ -50,6 +51,7 @@ render(){
       <Switch>
            <Route exact path='/' component={HomePage}/> 
            <Route path='/shop' component={ShopPage}/> 
+           <Route path='/contact' component={Contact}/> 
            <Route exact path='/signin' render = {()=>this.props.currentUser? (<Redirect to= '/'/>) : (<SignInAndSignUpPage/>)}/> 
       </Switch>
     </div>
---
+++ src/pages/contact/contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Contact extends React.Component {
+render() {
+
+return <div>
+    <h1>Ashenafi Tesfaye </h1>
+    <p>Email: ashenafitesfaye883@gmail.com</p>
+    <p>Mobile: +1571 461 9579</p>
+    <p>Twitter: coming soon </p>
+    </div>
+}
+}
+export default Contact
